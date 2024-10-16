a26af391eb1e8ad76621b855feb6f3e26c074e0c
José Irizarry Ayala <96902097+jirizarryayala@users.noreply.github.com>
2024-10-16 10:15:33 -0500

    Update RNASeq.R

--- General Use/RNASeq.R
+++ General Use/RNASeq.R
@@ -2,6 +2,10 @@
 ###Author: jirizarry
 ###Date: 10/11/2024
 
+###Note: This code is mean to be run on Tulane University's High Performance Computing cluster Cypress using R version 4.3.2
+###This requires use of the centos7 partition!
+###Example: idev --time=0-05:00:00 --partition=centos7
+
 ##Load packages, will install themselves if necessary
 if(!require("data.table")){
   install.packages("data.table")
@@ -13,17 +17,36 @@ if(!require("edgeR")){
   BiocManager::install("edgeR")
 }
 
-##Read in data
-DEG=fread("/path/to/sample")
+##Example with simple comparison
+DGE=fread("/path/to/sample") #Read in data
 sampleGroups=factor(c("wt", "wt", "s", "s", "wt", "s")) #Define grouping structure here,
-#exmaple if samples alternate between male and female group=c("male", "female", "male", "female")
-DEG1=DGEList(DEG, group = sampleGroups)
-expressedDEG=filterByExpr(DEG1) #Find unexpressed genes
-#table(expressedDEG) can show how many genes are expressed or not
-DEG2=DEG1[expressedDEG,,keep.lib.size=FALSE] #Remove unexpressed genes
-DEG3=normLibSizes(DEG2)
+#example if samples alternate between male and female group=c("male", "female", "male", "female")
+DGE1=DGEList(DGE, group = sampleGroups)
+expressedDGE=filterByExpr(DGE1) #Find unexpressed genes
+#table(expressedDGE) can show how many genes are expressed or not
+DGE2=DGE1[expressedDGE,,keep.lib.size=FALSE] #Remove unexpressed genes
+DGE3=normLibSizes(DGE2)
+DGE3=estimateDisp(DGE3)
 modelDesign=model.matrix(~sampleGroups)
-fit = glmQLFit(DEG3, modelDesign) #Fit inverse binomial model
+fit = glmQLFit(DGE3, modelDesign) #Fit inverse binomial model
 qlf=glmQLFTest(fit) #Significance test
 hits=topTags(qlf) #Selects top hits
-hits$table$gene #Returns top hits
+hits$table$Gene #Returns top hits
+
+##Example with multiple comparisons
+DGE=fread("/path/to/sample")
+sampleGroups=factor(c("wt", "s1", "s2", "s1", "wt", "s2"))
+DGE1=DGEList(DGE, group = sampleGroups)
+expressedDGE=filterByExpr(DGE1)
+DGE2=DGE1[expressedDGE,,keep.lib.size=FALSE]
+DGE3=normLibSizes(DGE2)
+DGE3=estimateDisp(DGE3)
+modelDesign=model.matrix(~0+sampleGroups, data=DGE3$samples)
+colnames(modelDesign)=levels(DGE3$samples$group)
+fit = glmQLFit(DGE3, modelDesign)
+contrasts=makeContrasts(A=s1-wt, B=s2-wt, C=s1-s2,levels=modelDesign)#Here we define what comparisons we are able to do later on
+qlf.A=glmQLFTest(fit,contrast=contrasts[,"A"])
+resultsA=topTags(qlf.A)
+qlf.B=glmQLFTest(fit,contrast=contrasts[,"B"])
+resultsB=topTags(qlf.B)
+qlf.C=glmQLFTest(fit,contrast=contrasts[,"C"])
